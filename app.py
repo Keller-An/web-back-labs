f055bd592bd839f30e11c92a37d8fb68f53279df
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-09-10 17:39:17 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/web")
-def start():
-    return """<!doctype html> \
-        <html> \
-           <body> \
-               <h1>web-сервис на flask</h1> \
-           </body> \
+def web():
+    return """<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервис на flask</h1>
+               <a href="/author">author</a>
+           </body>
         <html>"""
+
+@app.route("/author")
+def author():
+    name = "Махмадбекова Анастасия Эргашевна"
+    group = "ФБИ-32"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+           <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
