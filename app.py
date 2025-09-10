aa4ef63833bb2cf1ac56ded88e417a61d2f12d1e
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-09-10 16:40:33 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервис на flask"
-    
+    return "<!doctype html>" \
+        "<html>" \
+        "   <body>" \
+        "       <h1>web-сервис на flask</h1>" \
+        "   </body>" \
+        "<html>"
