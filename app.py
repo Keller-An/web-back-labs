73ea00e844988eff1711a2b7a5095a9ead9419e9
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-09-23 10:59:09 +0700

    Главная страница

--- app.py
+++ app.py
@@ -7,6 +7,61 @@ def not_found(err):
     return "Такой страницы не существует :(", 404
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f9f9f9;
+                margin: 0;
+                padding: 0;
+            }
+            header {
+                background-color: #2e8b57;
+                color: white;
+                padding: 20px;
+                text-align: center;
+            }
+            nav {
+                margin: 20px;
+            }
+            nav li {
+                margin: 10px 0;
+            }
+            nav a {
+                color: green;
+                font-weight: bold;
+            }
+            footer {
+                background-color: #333333;
+                color: white;
+                padding: 10px;
+                text-align: center;
+                margin-top: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
+        </header>
+
+        <nav>
+            <li><a href="/lab1">Лабораторная 1</a></li>
+        </nav>
+
+        <footer>
+            &copy; 2025 Махмадбекова Анастасия Эргашевна, ФБИ-32, 3 курс
+        </footer>
+    </body>
+</html>
+'''
+
 @app.route("/lab1/web")
 def web():
     return """<!doctype html>
