9df72490ac7c8599c64a27cb53af4503b245757b
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-11-08 22:23:02 +0700

    исправление SQL-иньекции

--- lab5.py
+++ lab5.py
@@ -41,7 +41,7 @@ def login():
     
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
 
     if not user:
@@ -72,14 +72,14 @@ def register():
 
     conn, cur = db_connect()
 
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html',
                                error="Такой пользователь уже существует!")
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
     conn.commit() 
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
