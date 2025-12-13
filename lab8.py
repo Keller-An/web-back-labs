63ec917926cd1cd9f60083d796129904fb138e37
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-12-13 20:00:51 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, abort, jsonify
+from flask import Blueprint, render_template, request, redirect
+from werkzeug.security import generate_password_hash
+from db import db
+from db.models import users, articles
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -12,10 +15,32 @@ def main():
 def login():
     return ''
 
-@lab8.route('/register')
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
 def register():
-    return ''
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
 
+    if not login_form or login_form.strip() == '':
+        return render_template('lab8/register.html',
+                               error = 'Имя пользователя не может быть пустым!', login_form = login_form)
+    
+    if not password_form or password_form.strip() == '':
+        return render_template('lab8/register.html',
+                               error = 'Пароль не может быть пустым!', login_form = login_form)
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html',
+                               error = 'Такой пользователь уже существует', login_form = login_form)
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
 
 @lab8.route('/list')
 def list():
