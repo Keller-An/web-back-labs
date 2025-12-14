11b34747ea512d0b7781b07311f1d965fbbd8aa9
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-12-14 23:22:48 +0700

    исправления в blueprint

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
-app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz)
 visit_log = []
 
