1d0b475ba88b0932b0e91277a8c468d14ade9903
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-12-14 23:17:48 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8 
+from lab9 import lab9 
 from rgz import rgz  
 
 app = Flask(__name__)
@@ -55,6 +56,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 app.register_blueprint(lab8)
 app.register_blueprint(rgz)
 visit_log = []
@@ -199,6 +201,7 @@ def index():
               <li><a href="/lab6/">Лабораторная 6</a></li>
               <li><a href="/lab7/">Лабораторная 7</a></li>
               <li><a href="/lab8/">Лабораторная 8</a></li>
+              <li><a href="/lab9/">Лабораторная 9</a></li>
               <li><a href="/rgz/">Расчетно-графическое задание</a></li>
         </nav>
 
